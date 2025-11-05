2f1f64580f93cf3f62023b7549c46ffa78c1cc9b
Liam <g00435609@atu.ie>
2025-11-05 21:49:30 +0000

    Added A search bar and a search button

--- LiamKapelianis_PokemonBuilder/MainPage.xaml.cs
+++ LiamKapelianis_PokemonBuilder/MainPage.xaml.cs
@@ -9,17 +9,12 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void SearchButton_Clicked(object sender, EventArgs e)
+        { 
+            
         }
+
+
     }
 
 }
