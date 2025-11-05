a6d0b60f5092cf41d5e9c719245e678aa126d25f
Liam <g00435609@atu.ie>
2025-11-05 22:39:12 +0000

    Added a grid,added taprecognizer code that will be implented at a later stage to change colour of border of grid

--- LiamKapelianis_PokemonBuilder/MainPage.xaml.cs
+++ LiamKapelianis_PokemonBuilder/MainPage.xaml.cs
@@ -1,20 +1,66 @@
-﻿namespace LiamKapelianis_PokemonBuilder
+﻿namespace LiamKapelianis_PokemonBuilder;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    //variables 
+    int count = 0;
+    private int numbRows = 3;
+    private int player = 1;
+
+    public MainPage()
     {
-        int count = 0;
-
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        private void SearchButton_Clicked(object sender, EventArgs e)
-        { 
-            
-        }
-
+        InitializeComponent();
+        //Calls the grid holdimg pokeon sprites and names and types as soon as app starts
+        TheGrid();
+    }
+    //Searches for the pokemon typed into the search bar
+    private void SearchButton_Clicked(object sender, EventArgs e)
+    {
 
     }
+    //the grid that will be created holding the pokemon(PokeShowcase)
+    private void TheGrid()
+    {
+        //Create numbRows rows and numbrows columns
+        for (int i = 0; i < numbRows; ++i)
+        {
+            PokeShowcase.AddRowDefinition(new RowDefinition());
+            PokeShowcase.AddColumnDefinition(new ColumnDefinition());
+        }
+
+        //Populate the grid with Borders
+        for (int i = 0; i < numbRows; ++i)
+        {
+            for (int j = 0; j < numbRows; ++j)
+            {
+                Border styledBorder = new Border
+                {
+                    BackgroundColor = Colors.Red,
+                    Stroke = Colors.Black,
+                    StrokeThickness = 3
+
+                };
+                TapGestureRecognizer tapGestureRecognizer = new TapGestureRecognizer();
+                tapGestureRecognizer.Tapped += OnBorderTapped;
+                styledBorder.GestureRecognizers.Add(tapGestureRecognizer);
+                PokeShowcase.Add(styledBorder, j, i);
+            }
+        }
+    }
+
+    private void OnBorderTapped(object? sender, TappedEventArgs e)
+    {
+        Border border = (Border)sender;
+        if (border != null)
+        {
+          // makeAnimation();
+        }
+    }
+
+   /* private void makeAnimation(Border border)
+    {
+        
+    }*/
+
 
 }
