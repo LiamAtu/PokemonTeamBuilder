6ca43a036b06e6dd650082a2579e71e71dc77381
Liam <g00435609@atu.ie>
2025-11-11 12:20:04 +0000

    Changed the grid to a collection view so it is eaier to store the api and to fill it to fit the page,added a fram around it with cornered border so it look nice,implemented the pokeAPI and got the colour of the pokemon cells to match the type collour of the pokemon,createf an ontap gesture for future use,search bar button does not work

--- LiamKapelianis_PokemonBuilder/MainPage.xaml.cs
+++ LiamKapelianis_PokemonBuilder/MainPage.xaml.cs
@@ -1,66 +1,143 @@
-﻿namespace LiamKapelianis_PokemonBuilder;
+﻿using System.Net.Http.Json;
+using System.Windows.Input;
+
+namespace LiamKapelianis_PokemonBuilder;
 
 public partial class MainPage : ContentPage
 {
-    //variables 
-    int count = 0;
-    private int numbRows = 3;
-    private int player = 1;
+    //requeast to the pokemon api (read only so wee dont change the data,just grab it)
+    private readonly HttpClient _httpClient = new();
 
     public MainPage()
     {
         InitializeComponent();
-        //Calls the grid holdimg pokeon sprites and names and types as soon as app starts
-        TheGrid();
-    }
-    //Searches for the pokemon typed into the search bar
-    private void SearchButton_Clicked(object sender, EventArgs e)
-    {
+        //needed so binding background and binding name can work
+        BindingContext = this;
 
-    }
-    //the grid that will be created holding the pokemon(PokeShowcase)
-    private void TheGrid()
-    {
-        //Create numbRows rows and numbrows columns
-        for (int i = 0; i < numbRows; ++i)
-        {
-            PokeShowcase.AddRowDefinition(new RowDefinition());
-            PokeShowcase.AddColumnDefinition(new ColumnDefinition());
-        }
+        //creates a command that runs when a pokemnon frtame is tapped
+        PokemonTappedCommand = new Command<PokemonHolder>(OnPokemonTapped);
 
-        //Populate the grid with Borders
-        for (int i = 0; i < numbRows; ++i)
-        {
-            for (int j = 0; j < numbRows; ++j)
-            {
-                Border styledBorder = new Border
-                {
-                    BackgroundColor = Colors.Red,
-                    Stroke = Colors.Black,
-                    StrokeThickness = 3
-
-                };
-                TapGestureRecognizer tapGestureRecognizer = new TapGestureRecognizer();
-                tapGestureRecognizer.Tapped += OnBorderTapped;
-                styledBorder.GestureRecognizers.Add(tapGestureRecognizer);
-                PokeShowcase.Add(styledBorder, j, i);
-            }
-        }
+        // loads pokemon on startup
+        _ = LoadThePokemonAsync();
     }
 
-    private void OnBorderTapped(object? sender, TappedEventArgs e)
-    {
-        Border border = (Border)sender;
-        if (border != null)
-        {
-          // makeAnimation();
-        }
-    }
+    //tap actionfor the collection view items
+    public ICommand PokemonTappedCommand { get; }
 
-   /* private void makeAnimation(Border border)
+    private void OnPokemonTapped(PokemonHolder pokemon)
     {
+        // Example: swap background color by player
         
-    }*/
 
+        //forces a UI refresh for user
+        PokeShowcaseGrid.ItemsSource = PokeShowcaseGrid.ItemsSource;
+    }
 
+    // This displays a single pokemon's info-name,sprite,type and the bg colour
+    public class PokemonHolder
+    {
+        public string Name { get; set; }
+        public string Sprite { get; set; }
+        public Color BackgroundColor { get; set; } = Colors.Red;
+        public List<string> Types { get; set; } = new();
+    }
+
+    // PokéAPI
+    public class PokemonResponse
+    {
+        public string name { get; set; }
+        public Sprites sprites { get; set; }
+        public List<TypeSlot> types { get; set; }
+    }
+
+    public class Sprites
+    {
+        public string front_default { get; set; }
+    }
+
+    public class TypeSlot
+    {
+        public TypeInfo type { get; set; }
+    }
+
+    public class TypeInfo
+    {
+        public string name { get; set; }
+    }
+
+    private async Task LoadThePokemonAsync()
+    {
+        var allPokemon = new List<PokemonHolder>();
+
+        for (int i = 1; i <= 9; i++) // the higher the number the more pokemon to load
+        {
+            string url = $"https://pokeapi.co/api/v2/pokemon/{i}";
+            try
+            {
+                var p = await _httpClient.GetFromJsonAsync<PokemonResponse>(url);
+                if (p != null)
+                {
+                    allPokemon.Add(new PokemonHolder
+                    {
+                        Name = char.ToUpper(p.name[0]) + p.name.Substring(1),
+                        Sprite = p.sprites.front_default,
+                        Types = p.types.Select(t => t.type.name).ToList(),
+                        BackgroundColor = GetTypeColor(p.types.FirstOrDefault()?.type.name)
+                    });
+                }
+            }
+            catch { }
+        }
+
+        PokeShowcaseGrid.ItemsSource = allPokemon;
+    }
+    //Maps a Pokémon’s main type to a colour.
+    private Color GetTypeColor(string type)
+    {
+        return type?.ToLower() switch
+        {
+            "fire" => Colors.OrangeRed,
+            "water" => Colors.DodgerBlue,
+            "grass" => Colors.Green,
+            "electric" => Colors.Yellow,
+            "psychic" => Colors.MediumPurple,
+            "ice" => Colors.LightCyan,
+            "dragon" => Colors.MediumBlue,
+            "dark" => Colors.Gray,
+            "fairy" => Colors.Pink,
+            "fighting" => Colors.Brown,
+            "poison" => Colors.Purple,
+            "ground" => Colors.SandyBrown,
+            "rock" => Colors.DarkGoldenrod,
+            "bug" => Colors.OliveDrab,
+            "ghost" => Colors.Indigo,
+            "steel" => Colors.LightGray,
+            "normal" => Colors.Beige,
+            _ => Colors.Red
+        };
+    }
+
+    //search bar button
+    private async void SearchButton_Clicked(object sender, EventArgs e)
+    {
+       /* string query = SearchBar.Text?.Trim().ToLower();
+        if (string.IsNullOrEmpty(query))
+        {
+            await DisplayAlert("Error", "Please enter a Pokémon name.", "OK");
+            return;
+        }
+
+        var pokemon = ((List<PokemonHolder>)PokeShowcaseGrid.ItemsSource)
+                      .FirstOrDefault(p => p.Name.ToLower() == query);
+
+        if (pokemon != null)
+        {
+            await DisplayAlert("Found!", $"{pokemon.Name} found!", "OK");
+            PokeShowcaseGrid.ScrollTo(pokemon);
+        }
+        else
+        {
+            await DisplayAlert("Not Found", $"No Pokémon named {query}", "OK");
+        }*/
+    }
 }
